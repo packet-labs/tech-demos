660cc8ba927a048bddb0e8f8bd6ec6005126e667
Cody Hill <15899666+c0dyhi11@users.noreply.github.com>
2020-04-01 12:57:49 -0500

    Update post_install.sh

--- simple-web-server/templates/post_install.sh
+++ simple-web-server/templates/post_install.sh
@@ -2,6 +2,6 @@
 yum install epel-release -y
 yum install nginx -y
 mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.bak
-echo '<center><div style="display:inline; white-space:nowrap;"><div style="display:inline; white-space:nowrap;"><p style="font-weight: bold;font-size: 100px;">Hello <img src="https://www.pngkey.com/png/full/321-3217317_verizon-logo-transparent-png-sticker-verizon-wireless-prepaid.png" alt="Hello Verizon!" style="height:100px;" />!</p></div></div></center>' > /usr/share/nginx/html/index.html
+echo '<center><div style="display:inline; white-space:nowrap;"><div style="display:inline; white-space:nowrap;"><p style="font-weight: bold;font-size: 100px;">Hello <img src="https://www.packet.com/packet-v4/images/logo-main@2x.png" alt="Hello Packet!" style="height:100px;" />!</p></div></div></center>' > /usr/share/nginx/html/index.html
 systemctl enable nginx
 systemctl start nginx
