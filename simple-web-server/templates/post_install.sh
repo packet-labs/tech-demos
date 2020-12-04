558f7204c1c0341d1590b3b7a81e568303888bd5
K Rain Leander <rainleander@users.noreply.github.com>
2020-12-04 16:59:08 +0100

    Delete post_install.sh

--- simple-web-server/templates/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum install epel-release -y
-yum install nginx -y
-mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.bak
-echo '<center><div style="display:inline; white-space:nowrap;"><div style="display:inline; white-space:nowrap;"><p style="font-weight: bold;font-size: 100px;">Hello <img src="https://www.packet.com/packet-v4/images/logo-main@2x.png" alt="Hello Packet!" style="height:100px;" />!</p></div></div></center>' > /usr/share/nginx/html/index.html
-systemctl enable nginx
-systemctl start nginx
